29938655dd8b867f03053f165139397af5af9114
Willy Picard <willy@picard.pl>
2014-04-26 23:19:44 +0200

    provided the implementation for the cached matrix inversion

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,47 @@
+makeVector <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
+}
+
+cachemean <- function(x, ...) {
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmean(m)
+  m
+}
+
 ## Put comments here that give an overall description of what your
 ## functions do
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  cache <- NULL
+  set <- function(y) {
+    x <<- y
+    cache <<- NULL
+  }
+  get <- function() x
+  setCache <- function(solve) cache <<- solve
+  getCache <- function() cache
+  list(set = set, get = get,
+       setCache = setCache,
+       getCache = getCache)
+  
 }
 
 
@@ -12,4 +49,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  cache <- x$getCache()
+  if(!is.null(cache)) {
+    message("getting cached data")
+    return(cache)
+  }
+  data <- x$get()
+  cache <- solve(data, ...)
+  x$setCache(cache)
+  cache
 }
